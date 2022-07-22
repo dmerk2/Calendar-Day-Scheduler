37b039957b0091065c1bbc7e62b9b18985f1e895
Daniel Merkin <dan.merkin@gmail.com>
2022-07-22 17:05:37 -0400

    created function to determine time of day

--- Develop/JS/script.js
+++ Develop/JS/script.js
@@ -1,87 +1,59 @@
-const calendar = $('#calendar');
-
-for (let i = 1; i <= 24; i++) {
-  console.log(i)
-} 
-
-let currentDay = moment().format('dddd, MMMM Do YYYY'); 
+// Get the current day and date from Moment.js
+let currentDay = moment().format('dddd, MMMM Do YYYY');
 $("#currentDay").text(currentDay)
 
-let startTime = moment().startOf('date').fromNow();
-$("#hour").text(time)
+// Take users input inside of the textarea and append it into textarea
+let newTask = $('textarea');
+$('textarea').text(newTask)
 
-let 
+let newTaskAdded = function inputNewTask (event) {
+  event.preventDefualt();
+  console.log('clcfgadick')
+  // userInput = event.concat('textarea', taskInput)
+  task.addClass('taskAdded', event);
+  task.appendTo(textarea);
+};
 
-// const store = window.localStorage;
+$('btn').click('submit', newTaskAdded, console.log('click'))
 
-// const container = $(".container");
+let past = moment().startOf('hour').fromNow(); 
+let present = moment().fromNow();
+let future = moment().endOf('hour').fromNow(); 
 
-// const now = moment();
+function timeOfDay() {
+  if (present) {
+    console.log('present')
+  } else if (present === past) {
+    console.log('current time')
+  } else if (present === future) {
+    console.log('future')
+  }
+}
 
-// const currentTime = { text: moment().format("h:00 A"), hour: moment().hour() };
+timeOfDay()
 
-// $("#day").text(now.format("dddd MMMM DD, YYYY"));
-
-// const range = (start, end, step) => {
-//   return Array.from(
-//     Array.from(Array(Math.ceil((end - start) / step)).keys()),
-//     (x) => start + x * step
-//   );
+// var printSkills = function (name, date) {
+//   var listEl = $('<li>');
+//   var listDetail = name.concat(' on ', date);
+//   listEl.addClass('list-group-item').text(listDetail);
+//   listEl.appendTo(skillsListEl);
 // };
 
-// const hoursOfTheDay = Array.from(new Array(24)).map((v, i) => {
-//   const text = moment().hour(i).format("h:00 A");
-//   const hour = moment().hour(i);
-//   return { text, hour };
-// // });
+// var handleFormSubmit = function (event) {
+//   event.preventDefault();
 
-// function color(time) {
-//   return time.text === currentTime.text
-//     ? "bg-red-300"
-//     : time.hour < now
-//     ? "bg-gray-300"
-//     : "bg-green-200";
-// }
+//   var nameInput = nameInputEl.val();
+//   var dateInput = dateInputEl.val();
 
-// hoursOfTheDay.forEach((hr) => {
-//   const grid = $(
-//     `<form data-name="${hr.text}" class="grid grid-cols-12  border-gray-500 "></form>.`
-//   );
+//   if (!nameInput || !dateInput) {
+//     console.log('You need to fill out the form!');
+//     return;
+//   }
 
-//   const time = $(
-//     `<div class="flex items-center justify-center col-span-2 h-16">${hr.text}</div>`
-//   );
+//   printSkills(nameInput, dateInput);
 
-//   const textArea = $(
-//     `<textarea name="${
-//       hr.text
-//     }" maxLength="50" style="resize: none; overflow: hidden;" class="col-span-8 h-16 p-6 ${color(
-//       hr
-//     )}">${store.getItem(hr.text) || ""}</textarea>`
-//   );
-
-//   textArea.keydown((e) => {
-//     if (e.keyCode == 13 && !e.shiftKey) {
-//       e.preventDefault();
-//       return false;
-//     }
-//   });
-
-//   const saveButton = $(
-//     `<button type="submit" class="col-span-2 h-16 bg-indigo-500 text-white font-bold hover:bg-indigo-400 transition duration-500 ease-in-out"><i class="fas fa-save text-xl"></i></button>`
-//   );
-
-//   grid.submit((e) => {
-//     e.preventDefault();
-
-//     const value = $(`textarea[name="${hr.text}"]`).val();
-
-//     store.setItem(hr.text, value);
-//   });
-
-//   grid.append(time);
-//   grid.append(textArea);
-//   grid.append(saveButton);
-
-//   container.append(grid);
-// });
+//   // resets form
+//   nameInputEl.val('');
+//   dateInputEl.val('');
+// };
+// formEl.on('submit', handleFormSubmit);
